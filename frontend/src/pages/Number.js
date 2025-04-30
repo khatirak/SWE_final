72d9beffa3fa58ebccf286b40f6ddd7211cb4486
khatirak <kk4827@nyu.edu>
2025-04-30 18:27:29 +0400

    Refactor update_listing_status endpoint to improve error handling and response structure

---
+++ frontend/src/pages/Number.js
@@ -0,0 +1,3 @@
+// here is where users will be directed to after they login and there is no number assoicated to their account
+// need ot have only digits and  '+' be accpetable - use regex
+// this page should only ever appear once, wehen the user logins for the very forst timre and then the db field number should not mempty from this point
